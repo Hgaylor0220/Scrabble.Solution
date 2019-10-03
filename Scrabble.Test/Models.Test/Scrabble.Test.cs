fdc154231dcb18bc5474758967e2a4abe47226c7
Student <example@example.com>
2019-10-03 13:20:30 -0700

    add second test and dispose method to clear score before tests are run.

--- Scrabble.Test/Models.Test/Scrabble.Test.cs
+++ Scrabble.Test/Models.Test/Scrabble.Test.cs
@@ -1,14 +1,19 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Game.Models;
+using System;
 
 namespace Game.Test
 {
     [TestClass]
-    public class ScrabbleTest
-    {
-        
+    public class ScrabbleTest : IDisposable
+   {    
+       public void Dispose()
+        {
+            Scrabble.ClearAll();
+        }
+
         [TestMethod]
-        public void Scrabble_CanUserEnterAWord_A()
+        public void WordScore_CanUserEnterALetter_A()
         {
             //Arrange
             string input = "a";
@@ -16,6 +21,14 @@ namespace Game.Test
             int result = Scrabble.WordScore(input);
             //Assert
             Assert.AreEqual(1, result);
+        }
+
+        [TestMethod]
+        public void WordScore_CanUserEnterAWord_Hello()
+        {
+            string input = "hi";
+            int result = Scrabble.WordScore(input);
+            Assert.AreEqual(5, result);
         }
     }
 }
--- Scrabble/Models/Scrabble.cs
+++ Scrabble/Models/Scrabble.cs
@@ -6,14 +6,11 @@ namespace Game.Models
 {
     public class Scrabble
     {
-
         public static Dictionary<char,int> scrabble = new Dictionary<char,int>(){{'a',1},{'e',1},{'i',1},{'o',1},{'u',1},{'l',1},{'n',1},{'r',1},{'s',1},{'t',1},{'d',2},{'g',2},{'b',3},{'c',3},{'m',3},{'p',3},{'f',4},{'h',4},{'v',4},{'w',4},{'y',4},{'k',5},{'j',8},{'x',8},{'q',10},{'z',10}};
 
-        
         public static char[] Input { get; set;}
         static int Score = 0;
 
-          
         public static int WordScore(string input)
         {
             Input = input.ToCharArray();
@@ -27,7 +24,9 @@ namespace Game.Models
             }
             return Score;
         }
-
-        
+        public static void ClearAll()
+        {
+            Score = 0;
+        }
     }
 }
